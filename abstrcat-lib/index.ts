d89414f908adc0fab5ee66acb1999f2a145f6561
Maksym Osovitnii <maksym.osovitnii@neptuness.com>
2025-01-03 17:12:07 +0200

    structure setup

--- abstrcat-lib/index.ts
+++ abstrcat-lib/index.ts
@@ -1,3 +1,45 @@
-export const lib = () => {
-	console.log('lib');
-};
+export * as compiler from './compiler';
+export * as imports from './imports';
+export * as reactivity from './reactivity';
+export * as template from './template';
+export * as router from './router';
+export * as directives from './directives';
+export * as stores from './stores';
+export * as middleware from './middlewares';
+
+class AbstrCat {
+  constructor(
+    private readonly compiler: any,
+    private readonly imports: any,
+    private readonly reactivity: any,
+    private readonly template: any,
+    private readonly router: any,
+    private readonly directives: any,
+    private readonly stores: any,
+    private readonly middleware: any
+  ) {
+    this.compiler = compiler;
+    this.imports = imports;
+    this.reactivity = reactivity;
+    this.template = template;
+    this.router = router;
+    this.directives = directives;
+    this.stores = stores;
+    this.middleware = middleware;
+  }
+
+  start() {}
+}
+
+let started = false;
+
+export function createApp() {
+  const framework = new AbstrCat();
+
+  if (started) {
+    console.log('Abstrcat framework is already initialised');
+    return;
+  }
+
+  framework.start();
+}
